b10c12a4d3a07602467ca9261bdefea4b3da7684
gracetownland <speak2ayushsrihari@gmail.com>
2024-11-10 11:12:37 -0800

    template code

--- voice-buddy/src/App.js
+++ voice-buddy/src/App.js
@@ -1,25 +1,14 @@
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
+export default function Form() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="form-container">
+      <label>
+        Link of song
+        <input name="firstName" />
+      </label>
+      <hr />
+      <button type="submit">Go!</button>
     </div>
   );
 }
-
-export default App;
